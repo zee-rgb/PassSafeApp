b61524a66e90634322612908bb4ae36f0daac4b8
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-09-08 20:52:36 +1000

    Make byte_size migration idempotent

--- db/migrate/20250909000002_add_byte_size_to_solid_cache_entries.rb
+++ db/migrate/20250909000002_add_byte_size_to_solid_cache_entries.rb
@@ -1,5 +1,8 @@
 class AddByteSizeToSolidCacheEntries < ActiveRecord::Migration[8.0]
   def change
-    add_column :solid_cache_entries, :byte_size, :integer, null: true
+    # Only add the column if it doesn't already exist
+    unless column_exists?(:solid_cache_entries, :byte_size)
+      add_column :solid_cache_entries, :byte_size, :integer, null: true
+    end
   end
 end
