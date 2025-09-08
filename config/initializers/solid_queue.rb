7175182faf8338140964880744fc7012e0293015
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-09-08 11:11:24 +1000

    Fix duplicate SolidQueue gem entry

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,10 +1,10 @@
-require "solid_queue"
-
-SolidQueue.configure do |config|
-  config.connects_to = {
-    database: {
-      writing: :primary,
-      reading: :primary_replica
+if defined?(SolidQueue) && SolidQueue.respond_to?(:configure)
+  SolidQueue.configure do |config|
+    config.connects_to = {
+      database: {
+        writing: :primary,
+        reading: :primary_replica
+      }
     }
-  }
+  end
 end
