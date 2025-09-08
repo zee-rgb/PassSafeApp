fa72f4366d68dfbee6782910b169686172b0867b
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-09-08 19:30:36 +1000

    refactor(db): remove cache tables and update schema
    - Drop CreateSolidCacheTables migration
    - Update schema version
    - Add solid_cache_tables to database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_09_000000) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_09_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -37,6 +37,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_000000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_entries_on_user_id"
+  end
+
+  create_table "solid_cache_tables", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
